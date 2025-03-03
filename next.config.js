4ef0047f6d0b7b233aadcba5123bbc7dd10c3171
sahara <mrjung72@gmail.com>
2025-03-03 15:30:00 +0900

    add init sources

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 module.exports = {
+  env: {
+    CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
-        pathname: "/my-account/**",
+        pathname: "/dy3ebqwrk/**",
         search: "",
       },
     ],
